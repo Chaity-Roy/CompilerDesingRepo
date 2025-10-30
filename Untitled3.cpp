549b2716e636f8ab21a07aab151171044bcb0c6c
Chaity-Roy <127714190+Chaity-Roy@users.noreply.github.com>
2025-10-30 12:37:59 +0600

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void newFunction()
+{
+cout << "I am learning function";
+}
+
+int main() 
+{
+  newFunction();
+  cout << endl;
+  newFunction();
+  return 0;
+}
