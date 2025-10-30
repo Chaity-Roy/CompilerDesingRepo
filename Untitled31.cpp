297ee8d05d96b9eec2a88f59d31254e87620d4c7
Chaity-Roy <127714190+Chaity-Roy@users.noreply.github.com>
2025-10-30 12:35:49 +0600

    first commit

---
+++ Untitled31.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int numbers[2][2];
+    cout<<"enter 4 numbers: "<<endl;
+    for (int i = 0; j < 2; i++){
+        for(int j=0; j<2; j++){
+            cin>>numbers[i][j]
+        }
+    }
+    return 0;
+}
